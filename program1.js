af1e156eff99fff4d8d4612ee6889974b64c89bb
VARUNKUMARSINGH1133 <singhvarun8260@gmail.com>
2024-10-07 15:20:21 +0530

    VARUNKUMARSINGH

--- program1.js
+++ program1.js
@@ -2,10 +2,29 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
 
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (mapping[char]) {
+            let topElement = stack.length === 0 ? '#' : stack.pop();
+            if (topElement !== mapping[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
 module.exports = { isValid };
 
 
--- program2.js
+++ program2.js
@@ -3,8 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
-};
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
 
+        const currentVal = romanMap[s[i]];
+        const nextVal = romanMap[s[i + 1]];
+
+        if (currentVal < nextVal) {
+            total -= currentVal;
+        } else {
+            
+            total += currentVal;
+        }
+    }
+
+    return total;
+};
 
 module.exports={romanToInt}
